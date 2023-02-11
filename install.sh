3efff28db4d00d95319af8136afb7febbea27456
Amanda Tomlinson <actomlin@ucsd.edu>
2023-02-11 14:32:19 -0800

    add global gitignore

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf $(pwd)/vim/.vim/colors/iceberg.vim ~/.vim/colors/iceberg.vim
 ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/bash/.bashrc ~/.bashrc
 ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
+ln -sf $(pwd)/git/gitignore ~/.gitignore
 
 # file specific vim configs. right now this is just to set explicit tabs in
 # Makefiles
